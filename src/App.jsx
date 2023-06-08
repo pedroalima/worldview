cbac8fc4fad3bdb5c9a4e62fae1a08a97bd8f4b4
Pedro Lima <tsc.pedrolima@gmail.com>
2023-06-07 21:58:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+      <FontAwesomeIcon icon={faEnvelope} />
+    </div>
+  )
+}
+
+export default App
