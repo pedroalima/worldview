120b119edb9ddb0cd8a8e777dd0e2016dfe95e01
Pedro Lima <tsc.pedrolima@gmail.com>
2023-06-09 16:19:17 -0300

    style: add form section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,44 @@
 import '../style/components/form.sass'
+import { IoIosArrowDown } from "react-icons/io";
+import { BiSearch } from "react-icons/bi";
+import { useState } from 'react';
 
 export default function Form(){
+    const [ box, setBox ] = useState(false);
+
+    const handleClick = () => {
+        setBox(prev => !prev)
+    }
+
     return (
         <form className='form'>
-            <input className='form-input' placeholder='Search for a country...' type="search" name="search" id="countries" />
-            {/* <select name="regions" id="regions">
-                <option disabled selected value="none">Filter by regions</option>
-                <option value="Region">Region</option>
-                <option value="Region">Region</option>
-                <option value="Region">Region</option>
-            </select> */}
+            <div className='form-input'>
+                <BiSearch />
+                <input  
+                    placeholder='Search for a country...' 
+                    type="search" 
+                    name="search" 
+                    id="countries" 
+                />
+            </div>
+            <div className='form-select'>
+                <div onClick={handleClick} className='form-select-title'>
+                    <span>Filter by Regions</span>
+                    <IoIosArrowDown />
+                </div>
+                {box &&
+                <div className='form-select-box'>
+                    <div className='form-select-box-option'>
+                        <input 
+                            type="radio" 
+                            name="region" 
+                            id="region1" 
+                        />
+                        <label htmlFor="region1">Region</label>
+                    </div>
+                </div>
+                }
+            </div>
         </form>
     )
 }
